1d21d894509a0110072e616c944643328db77a65
KarrimorRifle <samuelto888@yahoo.co.uk>
2022-09-22 19:37:58 +0100

    dot env

--- myApp.js
+++ myApp.js
@@ -2,12 +2,15 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 app.use(express.static(__dirname + "/public"))
+require('dotenv').config()
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname + "/views/index.html")
 })
 
 app.get("/json", (req,res) => {
+    console.log(process.env.MESSAGE_STYLE)
+    res.send(process.env.MESSAGE_STYLE)
     res.json((process.env.MESSAGE_STYLE == "uppercase") ? {message: "HELLO JSON"} : {message: "Hello json"})
 })
 
@@ -18,30 +21,7 @@ app.get("/json", (req,res) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
  module.exports = app;
+//  app.listen(3000,() => {
+//     console.log("listening on port 3000")
+//  })
