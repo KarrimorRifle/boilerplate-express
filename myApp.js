3e039893e7979dac24e77c5ab88142c06a68ef1c
KarrimorRifle <samuelto888@yahoo.co.uk>
2022-09-22 19:00:22 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
+app.use('views', path.join(__dirname, 'views'))
 
 app.get('/', (req,res) => {
-    res.sendFile('/views/index')
+    res.sendFile('index')
 })
 
 
