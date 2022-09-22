664d4d4fb595595e82bbc0dec609575f00b7b0ec
KarrimorRifle <samuelto888@yahoo.co.uk>
2022-09-22 19:03:31 +0100

    path

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-app.use('views', path.join(__dirname, 'views'))
+path = __dirname + views/index.html;
 
 app.get('/', (req,res) => {
-    res.sendFile('index')
+    res.sendFile(path)
 })
 
 
